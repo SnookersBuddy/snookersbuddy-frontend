95ec58e3afc5e9c28fff02737c54f19d4cb8da79
Ruben Smidt <ruben@rsmidt.dev>
2023-04-02 14:24:27 +0200

    feat: adjust occupancy colors
    Prior, they were grey and purple. After feedback from servers, we want to make it green/red (hence more obvious).

--- src/global-types.d.ts
+++ src/global-types.d.ts
@@ -4,12 +4,16 @@ declare module "@mui/material/styles" {
   interface Theme {
     extraPalette: {
       cardBg: string;
+      success: string;
+      error: string;
     };
   }
   // allow configuration using `createTheme`
   interface ThemeOptions {
     extraPalette?: {
       cardBg?: string;
+      success?: string;
+      error?: string;
     };
   }
 }
